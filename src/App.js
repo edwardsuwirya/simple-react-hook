8eaaadef047f76d6964339f3171109d01a5feef0
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-15 14:32:51 +0700

    Simple React Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Hooks adalah fitur yang baru ada di React 16.8.
+//Fitur ini memungkinkan penggunaan state tanpa membuat class javascript
+const App = () => {
+    const [dark, setDark] = useState(false);
+    return (
+        <div style={{backgroundColor: dark ? 'black' : 'white'}}>
+            <p style={{color: dark ? 'white' : 'black'}}>Color : {dark ? 'black' : 'white'}</p>
+            <button onClick={() => setDark(!dark)}>
+                Set Theme {dark ? 'white' : 'black'}
+            </button>
+        </div>
+    );
 }
 
 export default App;
