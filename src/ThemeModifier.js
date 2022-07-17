e69ca8d2a70f9d6ae6f8ea4df6eb8075d543e463
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-17 13:27:49 +0700

    useEffect clean up, and skipping effect

--- src/ThemeModifier.js
+++ src/ThemeModifier.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 
 //useEffect, sebuah built in hook yang memiliki tujuan yang sama dengan
 // componentDidMount, componentDidUpdate, and componentWillUnmount
+// useEffect selalu dijalankan setiap render,tapi bisa diatur supaya tidak dijalankan setiap rendera
 const ThemeModifier = (props) => {
     const [dark, setDark] = useState(false);
 
@@ -13,7 +14,8 @@ const ThemeModifier = (props) => {
         return () => {
             console.log('Component will unmount')
         };
-    });
+        //Checking prevState vs currState, hanya akan dijalankan ketika berbeda
+    }, []);
     return (
         <div style={{backgroundColor: dark ? 'black' : 'white'}}>
             <p style={{color: dark ? 'white' : 'black'}}>Color : {dark ? 'black' : 'white'}</p>
