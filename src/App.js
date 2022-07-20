3127e841cd63f0307843c0ebd568087a882d7ce2
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-20 16:16:00 +0700

    moreEffect useEffect example

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import ThemeModifier from "./ThemeModifier";
 import DummyView from "./DummyView";
 import {useState} from "react";
+import MoreEffect from "./MoreEffect";
 
 //Hooks adalah fitur yang baru ada di React 16.8.
 //Fitur ini memungkinkan penggunaan state tanpa membuat class javascript
 const App = () => {
-    const [pageId, setPageId] = useState(0);
-    return (
-        pageId === 0 ? <ThemeModifier onNavigate={() => setPageId(1)}/> : <DummyView onNavigate={() => setPageId(0)}/>
-    );
+    // const [pageId, setPageId] = useState(0);
+    // return (
+    //     pageId === 0 ? <ThemeModifier onNavigate={() => setPageId(1)}/> : <DummyView onNavigate={() => setPageId(0)}/>
+    // );
+    return <MoreEffect/>
 }
 
 export default App;
---
+++ src/MoreEffect.js
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+import MoreEffectService from "./services/moreEffectService";
+
+const MoreEffect = () => {
+    const {doPrint} = MoreEffectService();
+    // Cara yang SALAH, result tidak akan tercetak
+    // let result = 'empty';
+    // useEffect(() => {
+    //     result = doPrint('1')
+    // });
+
+    // Hati", Backend akan di-flooding dengan request
+    // Karena useEffect == componentDidMount & ComponentDidUpdate,
+    // const [result, setResult] = useState('');
+    // useEffect(() => {
+    //     setResult(doPrint('1'))
+    //     console.log('1');
+    // });
+
+    // Cara yang banyak digunakan
+    // const [result, setResult] = useState();
+    // useEffect(() => {
+    //     setResult(doPrint('1'));
+    //     console.log('1');
+    // }, []);
+    //
+    // return (
+    //     <>
+    //         <h1>Print:{result}</h1>;
+    //     </>
+    // )
+
+    // useEffect akan Ddipanggil lagi ketika ada perubahan id
+    const [result, setResult] = useState('');
+    const [id, setId] = useState('');
+    useEffect(() => {
+        setResult(doPrint(id));
+        console.log('1');
+    }, [id]);
+
+    return (
+        <>
+            <button onClick={() => setId('12')}>Click</button>
+            <h1>Print:{result}</h1>;
+        </>
+    )
+
+
+}
+export default MoreEffect;
