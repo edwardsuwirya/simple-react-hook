35aa7c96bd39284b1a64453396112283dce0c458
edwardsuwirya <edward_suwirya@yahoo.com>
2022-08-06 07:16:18 +0700

    add some notes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 //Hooks adalah fitur yang baru ada di React 16.8.
 //Fitur ini memungkinkan penggunaan state tanpa membuat class javascript
 const App = () => {
+    //Gunakan hook di paling atas, sebelum react function yang lainnya (sebelum return)
     const [dark, setDark] = useState(false);
     return (
         <div style={{backgroundColor: dark ? 'black' : 'white'}}>
